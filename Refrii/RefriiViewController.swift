9e5283d5d17866da57dc8d1226a427c27119d108
jisung <jisung.smart@gmail.com>
2018-12-15 07:14:21 +0900

    change ViewController to RefriiViewController and add some code about array and tableView

---
+++ Refrii/RefriiViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Refrii
+//
+//  Created by The book on 15/12/2018.
+//  Copyright © 2018 The book. All rights reserved.
+//
+
+import UIKit
+
+class RefriiViewController: UITableViewController {
+    
+    let itemArray = ["first storage", "secondStorage", "ThirdStroage"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK: - TableView DataSource Method
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RefriiItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    
+    
+}
+
